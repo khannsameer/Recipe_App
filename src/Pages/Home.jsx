740226381a556d5e6b920e2a35a3bce2072b130d
khannsameer <sk8717108@gmail.com>
2025-07-12 20:24:23 +0530

    modified in app.jsx, home.jsx and header.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "../components/Header";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main className="w-full flex flex-col">
+      <Header />
+    </main>
+  );
 };
 
 export default Home;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { img1, img2, img3, img4, img5 } from "../Images";
+
+const images = [img1, img2, img3, img4, img5];
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="w-full h-[100vh]">
+      <div className="relative w-full h-full">
+        <img
+          src={images[Math.floor(Math.random() * images.length)]}
+          alt="Recipes"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
